c9b0b158c7f2ba0588b917a4dd5f99aaf7cefc7e
iorin-io <iodere77@gmail.com>
2025-01-20 22:52:51 +0900

    ユーザー作成APIを更新し、UUIDを生成してユーザーIDを管理するように変更。ユーザー情報の取得方法を修正し、メールアドレスでの重複チェックを実装。

--- src/app/api/[[...route]]/users.ts
+++ src/app/api/[[...route]]/users.ts
@@ -1,34 +1,38 @@
 import { z } from "zod";
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
-import { Bindings } from "./route";
+import { Bindings, Variables } from "./route";
+import { v4 } from "uuid";
 
 const postUserSchema = z.object({
-  user_id: z.string(),
   user_email: z.string().email(),
   user_name: z.string(),
 });
 
+const getUserSchema = z.object({
+  user_id: z.string().uuid(),
+});
+
 const users = new Hono<{
   Bindings: Bindings;
-  //  Variables: Variables
+  Variables: Variables;
 }>()
+  // POST /users
   .post("/", zValidator("json", postUserSchema), async (c) => {
     try {
-      const { user_id, user_email, user_name } = postUserSchema.parse(
+      const { user_email, user_name } = postUserSchema.parse(
         await c.req.json()
       );
 
       console.log("POST /users: Received data", {
-        user_id,
         user_email,
         user_name,
       });
 
       const { results } = await process.env.DB.prepare(
-        `SELECT * FROM users WHERE user_id = ?1`
+        `SELECT * FROM users WHERE user_email = ?1`
       )
-        .bind(user_id)
+        .bind(user_email)
         .all();
 
       if (results && results.length > 0) {
@@ -36,6 +40,11 @@ const users = new Hono<{
         return c.json({ status: "existing_user" });
       } else {
         console.log("POST /users: Inserting new user");
+
+        // ユーザーIDはランダムなUUIDを生成
+        // A require() style import is forbidden. Use dynamic imports instead.
+        const user_id = v4();
+
         await process.env.DB.prepare(
           `INSERT INTO users (user_id, user_email, user_name) VALUES (?1, ?2, ?3)`
         )
@@ -52,9 +61,9 @@ const users = new Hono<{
       return c.json({ error: err }, 500);
     }
   })
-  // GET /users/:user_id: ユーザー情報取得
+  // GET /users/:user_id
   .get("/:user_id", async (c) => {
-    const user_id = c.req.param("user_id");
+    const { user_id } = getUserSchema.parse(c.req.param("user_id"));
 
     const { results } = await process.env.DB.prepare(
       `SELECT * FROM users WHERE user_id = ?1`
@@ -68,7 +77,7 @@ const users = new Hono<{
 
     return c.json(results[0]);
   })
-  // GET /users: ユーザー一覧取得
+  // GET /users
   .get("/", async (c) => {
     const { results } =
       await process.env.DB.prepare(`SELECT * FROM users`).all();
