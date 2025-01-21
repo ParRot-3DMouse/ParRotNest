5f36dc265b20e72a9381ed89dff5354b40d2a09b
iorin-io <iodere77@gmail.com>
2025-01-21 10:31:24 +0900

    Keymapsテーブルを作成するマイグレーションを追加

---
+++ migrations/0002_create_keymaps.sql
@@ -0,0 +1,10 @@
+-- Migration number: 0002 	 2025-01-21T01:18:47.084Z
+CREATE TABLE IF NOT EXISTS keymaps (
+  keymap_id TEXT PRIMARY KEY,
+  keymap_name TEXT NOT NULL,
+  keymap_json TEXT NOT NULL,
+  user_id INTEGER NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
