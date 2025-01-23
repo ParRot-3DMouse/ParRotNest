cf759f88b439f85470770cbd70ecb23fdb280c7e
iorin-io <iodere77@gmail.com>
2025-01-23 17:31:01 +0900

    KeymapToShareインターフェースを追加し、共有キーマップの構造を定義

--- src/app/api/types.ts
+++ src/app/api/types.ts
@@ -16,3 +16,12 @@ export interface Keymap {
   created_at: string;
   updated_at: string;
 }
+
+export interface KeymapToShare {
+  share_id: string;
+  keymap_name: string;
+  keymap_json: KeymapCollection;
+  author_id: string;
+  created_at: string;
+  updated_at: string;
+}
