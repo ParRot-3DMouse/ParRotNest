778dbc5f12df867d4f0dc49f7098d5684a6b8707
iorin-io <iodere77@gmail.com>
2025-01-23 16:40:58 +0900

    keymaps_to_shareテーブルを作成するマイグレーションを追加

---
+++ migrations/0003_create_keymaps_to_share.sql
@@ -0,0 +1,12 @@
+-- Migration number: 0003 	 2025-01-23T07:29:59.278Z
+CREATE TABLE IF NOT EXISTS keymaps_to_share (
+  share_id TEXT PRIMARY KEY,
+  keymap_id TEXT NOT NULL,
+  keymap_name TEXT NOT NULL,
+  keymap_json TEXT NOT NULL,
+  author_id INTEGER NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (keymap_id) REFERENCES keymaps(keymap_id),
+  FOREIGN KEY (author_id) REFERENCES users(user_id)
+);
