bb60775ce38e56e99dbd11c66e7235b451910333
iorin-io <iodere77@gmail.com>
2025-01-28 14:54:37 +0900

    KeymapPageから未使用のuseEffectとuseRouterを削除し、接続デバイスがない場合の処理を簡素化

--- src/app/keymap/new/page.tsx
+++ src/app/keymap/new/page.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { KeymapCollection } from "../../../lib/device/types";
 import { initialState } from "../../../lib/device/reducer";
 import { KeymapComponent } from "../../../components/KeymapComponent";
 import { useHID } from "../../../components/provider/HIDContext";
-import { useRouter } from "next/navigation";
 import { DeviceCard } from "../../../components/DeviceCard";
 
 export default function KeymapPage() {
@@ -17,17 +16,6 @@ export default function KeymapPage() {
   });
   const [activeLayer, setActiveLayer] = useState<1 | 2 | 3>(1);
   const { connectedDevice, connect, disconnect } = useHID();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (connectedDevice === null) {
-      router.push("/");
-    }
-  }, [connectedDevice, router]);
-
-  if (connectedDevice === null) {
-    return null;
-  }
 
   return (
     <div>
